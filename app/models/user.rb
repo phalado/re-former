5782401f37df4400c3c1af2fd4e1be828c7a3a00
Phalado <phalado@gmail.com>
2019-12-10 13:45:49 -0300

    Created User model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :username, :email, :password, presence: true
+  validates :username, :email length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+end
